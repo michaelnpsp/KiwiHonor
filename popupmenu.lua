38c224f90dcf15f3ea72c7fe989ff0a89b0743d4
michaelsp <michael.npsp@gmail.com>
2025-04-23 02:26:42 +0200

    Refactor popupmenu code.

--- popupmenu.lua
+++ popupmenu.lua
@@ -2,89 +2,31 @@ local addonName, addonTbl = ...
 
 local lkm = LibStub("LibKiwiDropDownMenu-1.0", true)
 
--- localization
-local addon = _G[addonName]
-local L = addonTbl.L
-
--- default values
-local FONT_SIZE_DEFAULT = 12
-local ROW_TEXTURE_DEFAULT = "Interface\\Buttons\\WHITE8X8"
-local ROW_COLOR_DEFAULT = {.3,.3,.3,1}
+-- addon, localization
+local addon, L = _G[addonName], addonTbl.L
 
 -- addon.db
-local config, frame, stats
+local config, stats
 
 -- here starts the definition of the KiwiFrame menu
-local function cfgWidth(info)
-	frame.frameWidth = info.value~=0 and math.max(addon:GetWidth()+info.value, 50) or addon.defaults.frameWidth
-	addon:LayoutFrame()
-end
-local function cfgMargin(info)
-	frame.frameMargin = info.value~=0 and math.max( (frame.frameMargin or 4) + info.value, 0) or 4
-	addon:LayoutFrame()
-end
-local function cfgSpacing(info)
-	frame.spacing = info.value~=0 and math.max( frame.spacing + info.value, 0) or 1
-	addon.textLeft:SetText('')
-	addon.textRight:SetText('')
-	addon:LayoutFrame()
-	addon:UpdateContent()
-end
-local function cfgFontSize(info)
-	local font, size = addon:GetTextsFontInfo()
-	frame.fontSize = info.value~=0 and math.max( (size or FONT_SIZE_DEFAULT) + info.value, 5) or nil
-	addon:LayoutFrame()
-end
-local function cfgStrata(info,_,_,checked)
-	if checked==nil then return info.value == (frame.frameStrata or 'MEDIUM') end
-	frame.frameStrata = info.value~='MEDIUM' and info.value or nil
-	addon:LayoutFrame()
-end
-local function cfgAnchor(info,_,_,checked)
-	if checked==nil then return info.value == frame.framePos.anchor end
-	frame.framePos.anchor = info.value
-	addon:SavePosition()
-	addon:RestorePosition()
-end
 local function cfgDisplay(info,_,_,checked)
-	if checked==nil then return not config.display[info.value] end
-	config.display[info.value] = not config.display[info.value] or nil
+	if checked==nil then return not addon.db.profile.display[info.value] end
+	addon.db.profile.display[info.value] = not addon.db.profile.display[info.value] or nil
 	addon:LayoutFrame()
 	addon:UpdateContent()
 end
-local function cfgFont(info,_,_,checked)
-	if checked==nil then return info.value == (frame.fontName or '') end
-	frame.fontName = info.value~='' and info.value or nil
-	addon:LayoutFrame()
-	lkm:refreshMenu()
-end
-local function cfgBorder(info,_,_,checked)
-	if checked==nil then return info.value == (frame.borderTexture or '') end
-	frame.borderTexture = info.value~='' and info.value or nil
-	addon:LayoutFrame()
-	lkm:refreshMenu()
-end
-local function cfgRowTexture(info,_,_,checked)
-	if checked==nil then return info.value == (frame.rowTexture or '') end
-	frame.rowTexture = info.value~='' and info.value or nil
-	addon:LayoutFrame()
-	lkm:refreshMenu()
-end
-local function cfgColor(info, ...)
-	if select('#',...)==0 then return unpack( frame[info.value] or ROW_COLOR_DEFAULT ) end
-	frame[info.value] = {...}
-	addon:LayoutFrame()
-end
+
 local function cfgDetails(info,_,_,checked)
-	if checked==nil then return config.details~=nil end
-	local msg = config.details and
+	if checked==nil then return addon.db.profile.details~=nil end
+	local msg = addon.db.profile.details and
 				L["|cFF7FFF72KiwiHonor|r\nHonor stats will be displayed in a standalone window. Are you sure you want to disable KiwiHonor Details Plugin?"] or
 				L["|cFF7FFF72KiwiHonor|r\nHonor stats will be displayed in a Details window. Are you sure you want to enable KiwiHonor Details Plugin?"]
 	addon:ConfirmDialog(msg, function()
-		config.details = (not config.details) or nil
+		addon.db.profile.details = (not addon.db.profile.details) or nil
 		ReloadUI()
 	end)
 end
+
 local function cfgProfile(info,_,_,checked)
 	if checked==nil then return addon.db.profileName~='Default' end
 	local msg = addon.db.profileName=='Default' and
@@ -96,42 +38,37 @@ local function cfgProfile(info,_,_,checked)
 		ReloadUI()
 	end)
 end
+
 local function cfgToggleSession()
-	if stats.snTimeStart then
+	if addon.db.stats.snTimeStart then
 		addon:ConfirmDialog( L["|cFF7FFF72KiwiHonor|r\nAre you sure you want to finish the session?"], function() addon:FinishSession(); end)
 	else
 		addon:StartSession()
 	end
 end
+
 local function cfgSetHonorGoal()
-	addon:EditDialog(L['|cFF7FFF72KiwiHonor|r\nSet the Weekly Honor Goal:\n'], stats.wkHonorGoal or '', function(v)
-		stats.wkHonorGoal = tonumber(v) or nil
+	addon:EditDialog(L['|cFF7FFF72KiwiHonor|r\nSet the Weekly Honor Goal:\n'], addon.db.stats.wkHonorGoal or '', function(v)
+		addon.db.stats.wkHonorGoal = tonumber(v) or nil
 		addon:UpdateContent()
 	end)
 end
+
 local function getSessionText()
-	return stats.snTimeStart and L['Session Finish'] or L['Session Start']
+	return addon.db.stats.snTimeStart and L['Session Finish'] or L['Session Start']
 end
-local function isHideHidden()
-	return addon.plugin~=nil
-end
+
 local function cfgFrameHide(info,_,_,checked)
 	if checked==nil then return not not addon.dbframe.visible end
 	addon:ToggleFrameVisibility()
 end
+
 local function isPlugin()
 	return addon.plugin~=nil
 end
 
--- submenu size
-local menuSize = {
-	{ text = L['Higher (+)'],   value =  1 },
-	{ text = L['Smaller (-)'],  value = -1 },
-	{ text = L['Default'],      value =  0 },
-}
-
 -- menu main
-addon.menuMain = {
+local menuMain = {
 	{ text = L['Kiwi Honor [/khonor]'], isTitle = true },
 	{ text = getSessionText, func = cfgToggleSession },
 	{ text = L['Set Honor Goal'], func = cfgSetHonorGoal },
@@ -148,47 +85,16 @@ addon.menuMain = {
 		{ text = L['Honor remain'],     value = 'hr_remain',    },
 		{ text = L['Honor goal in'],    value = 'hr_goalin',    },
 	} },
-	{ text = L['Frame Strata'], hidden = isPlugin, default = { cf = cfgStrata, isNotRadio = false }, menuList = {
-		{ text = L['HIGH'],    value = 'HIGH',   },
-		{ text = L['MEDIUM'],  value = 'MEDIUM', },
-		{ text = L['LOW'],     value = 'LOW',  	 },
-	} },
-	{ text = L['Frame Anchor'], hidden = isPlugin, default = { cf = cfgAnchor, isNotRadio = false }, menuList = {
-		{ text = L['Top Left'],     value = 'TOPLEFT',     },
-		{ text = L['Top Right'],    value = 'TOPRIGHT',    },
-		{ text = L['Bottom Left'],  value = 'BOTTOMLEFT',  },
-		{ text = L['Bottom Right'], value = 'BOTTOMRIGHT', },
-		{ text = L['Left'],   		value = 'LEFT',   	   },
-		{ text = L['Right'],  		value = 'RIGHT',  	   },
-		{ text = L['Top'],    		value = 'TOP',    	   },
-		{ text = L['Bottom'], 		value = 'BOTTOM', 	   },
-		{ text = L['Center'], 		value = 'CENTER', 	   },
-	} },
-	{ text = L['Frame Width'],  hidden = isPlugin, default = { func = cfgWidth,    keepShownOnClick = 1 }, menuList = lkm:CopyTable(menuSize) },
-	{ text = L['Frame Border'], hidden = isPlugin, menuList = {
-		{ text = L['Border Texture'], menuList = lkm:defMediaMenu('border', cfgBorder) },
-		{ text = L['Border Color '],  hasColorSwatch = true, hasOpacity = true, value = 'borderColor', get = cfgColor, set = cfgColor },
-	} },
-	{ text = L['Frame Back'], hidden = isPlugin, menuList = {
-		{ text = L['Background color '], hasColorSwatch = true, hasOpacity = true, value = 'backColor', get = cfgColor, set = cfgColor },
-	} },
-	{ text = L['Text Margin'],  default = { func = cfgMargin,   keepShownOnClick = 1 }, menuList = lkm:CopyTable(menuSize) },
-	{ text = L['Text Spacing'], default = { func = cfgSpacing,  keepShownOnClick = 1 }, menuList = lkm:CopyTable(menuSize) },
-	{ text = L['Text Size'],    default = { func = cfgFontSize, keepShownOnClick = 1 }, menuList = lkm:CopyTable(menuSize) },
-	{ text = L['Text Font'], menuList = lkm:defMediaMenu('font', cfgFont, {[L['[Default]']] = ''}) },
-	{ text = L['Text Bars'], menuList = {
-		{ text = L['Bars Texture'], menuList = lkm:defMediaMenu('statusbar', cfgRowTexture, {[L['[None]']] = ''}) },
-		{ text = L['Bars Color'],   hasColorSwatch = true, hasOpacity = true, value = 'rowColor', get = cfgColor, set = cfgColor },
-	} },
-	{ text = L['Miscellaneus'], menuList = {
-		{ text = L['Details Plugin'], cf = cfgDetails, isNotRadio = true  },
-		{ text = L['Profile per Char'], cf = cfgProfile, isNotRadio = true },
-		{ text = L['Frame Visible'], cf =  cfgFrameHide, isNotRadio = true, hidden = isHideHidden },
-	} },
 }
-
--- show menu
-function addon:ShowMenu()
-	config, stats, frame = self.db.profile, self.db.stats, self.dbframe
-	lkm:showMenu(self.menuMain, "KiwiHonorPopupMenu", "cursor", 0 , 0, 2)
+for _,item in ipairs(addon.menuMain) do
+	menuMain[#menuMain+1] = item
 end
+menuMain[#menuMain+1] =	{ text = L['Miscellaneus'], menuList = {
+	{ text = L['Details Plugin'], cf = cfgDetails, isNotRadio = true  },
+	{ text = L['Profile per Char'], cf = cfgProfile, isNotRadio = true },
+	{ text = L['Frame Visible'], cf =  cfgFrameHide, isNotRadio = true, hidden = isPlugin },
+} }
+
+-- Register popup menu
+addon.menuMain = menuMain
+
